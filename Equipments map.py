8054ec25f3f941d7285a76040dec3840592dc5dc
javishphipt <javishphipy@gmail.com>
2019-08-27 15:55:30 +0700

    update

--- Equipments map.py
+++ Equipments map.py
@@ -5,24 +5,18 @@ import argparse
 import json
 import os
 from dotenv import load_dotenv
+from pyzabbix import ZabbixAPI
 
 
 __author__ = "Vishenka Philipp"
 __version__ = "1.0.0"
 
 
-load_dotenv('G:/python/conf/.env')
-config = {
-    'zabbix': {
-        'url': os.getenv('URL'),
-        'user': os.getenv('USER'),
-        'password': os.getenv('PASSWORD')
-    },
-}
+load_dotenv('../.env')
 
 
 def main():
-    # -hn "1300.21=BKM605.4-RB951G" -tp "[template] map" -sr = -
+    # -hn "1300.22=BKM67-MB3170" -tp "[template] map" -sr = -
     parser = argparse.ArgumentParser()
     parser.add_argument("-hn", "--hostname", type=str, help="zabbix hostname {HOST.NAME}")
     parser.add_argument("-tp", "--template", type=str, help="zabbix template map")
@@ -32,6 +26,22 @@ def main():
     sep = separation_hostname(args.hostname, args.separator)
     print(sep["map_name"])
     print(sep["number"])
+
+    zapi = ZabbixAPI(url=os.getenv('URL'),
+                     user=os.getenv('USER'),
+                     password=os.getenv('PASSWORD'))
+
+    params_maps = {
+        'output': 'extend'
+    }
+    maps = zapi.do_request(method='map.get',
+                           params=json.dumps(params_maps))['result']
+
+    params_hosts = {
+        'output': 'extend'
+    }
+    hosts = zapi.do_request(method='map.get',
+                            params=json.dumps(params_hosts))['result']
 
 
 def separation_hostname(hostname, separator):
